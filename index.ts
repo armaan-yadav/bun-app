865d30da50774e22edc5e936e4b3af4e8f22a82e
Armaan Yadav <yadavarmaan10@gmail.com>
2026-07-05 20:16:11 +0530

    changes

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  return res.json({ msg: "Hello from Server" });
+  return res.json({ msg: "Hello from Server v1" });
 });
 
 app.listen(PORT, () => {
