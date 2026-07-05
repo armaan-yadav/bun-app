fe5cba5ab2d46bffcaec960184a825819aecd14a
Armaan Yadav <yadavarmaan10@gmail.com>
2026-07-05 19:59:12 +0530

    init

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  return res.json({ msg: "Hello from Server" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
